e4eb1fa3428f37eddf6064e992b8293c350e1b5e
Guest007 <puramv307@gmail.com>
2023-06-11 08:58:45 +0530

    thymeleaf Examples

---
+++ Thymeleaf/Thymeleaf/src/main/java/com/example/Thymeleaf/controller/MyController.java
@@ -0,0 +1,24 @@
+package com.example.Thymeleaf.controller;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@Controller
+public class MyController {
+//    @GetMapping("/home")
+    @RequestMapping(value = "/home",method = RequestMethod.GET)
+    public String home(Model model){
+        model.addAttribute("name","vaishnav");
+        model.addAttribute("age",23);
+        model.addAttribute("city","bangaluru");
+        model.addAttribute("currentDate",new Date());
+        return "home";
+    }
+}
